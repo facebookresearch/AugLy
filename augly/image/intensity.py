d1edc5aa685dde0505c48625a6f6d53a9a612450
Tanuj Dhiman <56601466+tanujdhiman@users.noreply.github.com>
2021-07-03 19:03:30 +0530

    Update intensity.py

--- augly/image/intensity.py
+++ augly/image/intensity.py
@@ -43,7 +43,7 @@ def brightness_intensity(factor: float, **kwargs) -> float:
     return mult_factor_intensity_helper(factor)
 
 def skew_intensity(level: float, **kwargs):
-    return min(level * 100.0, 100.0)
+    return min(level, 100.0)
 
 def change_aspect_ratio_intensity(
     ratio: float, metadata: Dict[str, Any], **kwargs
