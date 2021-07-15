e5bcd1ddad5f48ca6121ca4c0b3ed5f5ef446f28
Tanuj Dhiman <56601466+tanujdhiman@users.noreply.github.com>
2021-07-15 22:56:59 +0530

    Update augly/image/transforms.py
    Co-authored-by: Joanna Bitton <joanna.bitton@gmail.com>

--- augly/image/transforms.py
+++ augly/image/transforms.py
@@ -1349,8 +1349,8 @@ class ShufflePixels(BaseTransform):
 class Skew(BaseTransform):
     def __init__(self, level: float = 0.5, axis: str = "x-axis", p: float = 1.0):
         """
-        @param level: the level of skew to apply to the image; a larger value means the skew
-            will be more intense. Recommended range is between [-2, 2]
+        @param level: the level of skew to apply to the image; a larger absolute value will
+            result in a more intense skew. Recommended range is between [-2, 2]
             
         @param axis: the axis along which the image will be skewed; can be 'x-axis' or 'y-axis'
         """
