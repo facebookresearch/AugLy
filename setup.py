fd3d40f897f0f48928c2a9efd31059b39b90953e
Joanna Bitton <joanna.bitton@gmail.com>
2021-07-21 12:33:43 -0700

    Update PyPI version (#89)
    Summary:
    ## Related Issue
    Fixes https://github.com/facebookresearch/AugLy/issues/86
    
    The newer versions of `nlpaug` seem to be a tad buggy -- to assure that no issues arise in our library, we're gonna keep the `nlpaug` version at 1.1.3. Note that you may have to additionally install `torch` to get text augmentations to work, despite the fact that it's unneeded in the code we use from `nlpaug`
    
    Pull Request resolved: https://github.com/facebookresearch/AugLy/pull/89
    
    Reviewed By: zpapakipos
    
    Differential Revision: D29799738
    
    Pulled By: jbitton
    
    fbshipit-source-id: 1620459c3d96dfedaa62d99b7e0d54763994dc1b

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ with open("README.md", encoding="utf8") as f:
 
 setuptools.setup(
     name="augly",
-    version="0.1.5",
+    version="0.1.6",
     description="A data augmentations library for audio, image, text, & video.",
     long_description=readme,
     long_description_content_type="text/markdown",
