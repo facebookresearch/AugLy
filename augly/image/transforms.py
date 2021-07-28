8bbb6b4a4b7bb9ed6480f48786963bf2d19468be
Tanuj Dhiman <56601466+tanujdhiman@users.noreply.github.com>
2021-07-28 21:48:15 +0530

    Update transforms.py
    Making axis variable as integer !!

--- augly/image/transforms.py
+++ augly/image/transforms.py
@@ -1312,12 +1312,12 @@ class ShufflePixels(BaseTransform):
 
     
 class Skew(BaseTransform):
-    def __init__(self, level: float = 0.5, axis: str = "x-axis", p: float = 1.0):
+    def __init__(self, level: float = 0.5, axis: int = 0, p: float = 1.0):
         """
         @param level: the level of skew to apply to the image; a larger absolute value will
             result in a more intense skew. Recommended range is between [-2, 2]
             
-        @param axis: the axis along which the image will be skewed; can be 'x-axis' or 'y-axis'
+        @param axis: the axis along which the image will be skewed; can be 0 for 'x-axis' or 1 for 'y-axis'
         """
         super().__init__(p)
         self.level = level
