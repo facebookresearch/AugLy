5e74201f1fff6f3e069fedeab50b2622e9348270
Zoe Papakipos <zoep@fb.com>
2021-09-08 04:17:50 -0700

    Move metadata funcs to dedicated file
    Differential Revision: D30639160
    
    fbshipit-source-id: 2392416e80d039ae3d041cd72a1725e0e123045e

---
+++ augly/image/utils/__init__.py
@@ -0,0 +1,29 @@
+from augly.image.utils.metadata import get_func_kwargs, get_metadata
+from augly.image.utils.utils import (
+    compute_stripe_mask,
+    compute_transform_coeffs,
+    get_template_and_bbox,
+    pad_with_black,
+    resize_and_pad_to_given_size,
+    ret_and_save_image,
+    rotated_rect_with_max_area,
+    scale_template_image,
+    square_center_crop,
+    validate_and_load_image,
+)
+
+
+__all__ = [
+    "get_func_kwargs",
+    "get_metadata",
+    "compute_stripe_mask",
+    "compute_transform_coeffs",
+    "get_template_and_bbox",
+    "pad_with_black",
+    "resize_and_pad_to_given_size",
+    "ret_and_save_image",
+    "rotated_rect_with_max_area",
+    "scale_template_image",
+    "square_center_crop",
+    "validate_and_load_image",
+]
