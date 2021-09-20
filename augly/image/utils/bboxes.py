00f23033dc4f546c61136a59c5a27d4ca0c0d796
Zoe Papakipos <zoep@fb.com>
2021-09-20 09:54:35 -0700

    Bbox computation for pad_square (3/12)
    Reviewed By: jbitton
    
    Differential Revision: D30638997
    
    fbshipit-source-id: 9ecb4798846dc71931e6233c6c8db9e587bce0fc

--- augly/image/utils/bboxes.py
+++ augly/image/utils/bboxes.py
@@ -19,3 +19,18 @@ def pad_bboxes_helper(bbox: Tuple, w_factor: float, h_factor: float, **kwargs) -
         (right_factor + w_factor) / new_w,
         (lower_factor + h_factor) / new_h,
     )
+
+
+def pad_square_bboxes_helper(bbox: Tuple, src_w: int, src_h: int, **kwargs) -> Tuple:
+    """
+    In pad_square, pad is called with w_factor & h_factor computed as follows, so we can
+    use the `pad_bboxes_helper` function to transform the bbox
+    """
+    w_factor, h_factor = 0, 0
+
+    if src_w < src_h:
+        w_factor = (src_h - src_w) / (2 * src_w)
+    else:
+        h_factor = (src_w - src_h) / (2 * src_h)
+
+    return pad_bboxes_helper(bbox, w_factor=w_factor, h_factor=h_factor)
