57c70095246e2fae2fcb851df6522f737ea0313d
Guillermo Sanchez <Sanguillermo98@gmail.com>
2021-10-04 18:11:23 -0400

    Update augly/image/functional.py
    Co-authored-by: Zoe Papakipos <zpapakipos@users.noreply.github.com>

--- augly/image/functional.py
+++ augly/image/functional.py
@@ -1012,11 +1012,10 @@ def opacity(
     """
     assert 0 <= level <= 1, "level must be a value in the range [0, 1]"
 
-    func_kwargs = imutils.get_func_kwargs(metadata, locals())
-    
-    src_mode = image.mode
-
     image = imutils.validate_and_load_image(image).convert(mode="RGBA")
+    
+    func_kwargs = imutils.get_func_kwargs(metadata, locals())
+    src_mode = image.mode
 
     mask = image.convert("RGBA").getchannel("A")
     mask = Image.fromarray((np.array(mask) * level).astype(np.uint8))
