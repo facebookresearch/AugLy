97f210e6a0e2a1af9407f61acdc3f1816b3d73ab
Adib234 <adibadib42@hotmail.com>
2021-10-11 16:02:26 -0400

    fixed more formatting nits because of merging

--- augly/tests/video_tests/transforms/composite_test.py
+++ augly/tests/video_tests/transforms/composite_test.py
@@ -83,7 +83,9 @@ class TransformsVideoUnitTest(BaseVideoUnitTest):
     def test_ReplaceWithBackground(self):
         self.evaluate_class(
             vidaugs.ReplaceWithBackground(
-                source_offset=0.1, background_offset=0, source_percentage=0.7,
+                source_offset=0.1, 
+                background_offset=0, 
+                source_percentage=0.7,
             ),
             fname="replace_with_background",
             metadata_exclude_keys=["dst_duration", "dst_fps"],
