e0bea83eb8844d86c43e563afd5ea1e976ed1b36
Aditya <adityaprasad9032@gmail.com>
2021-12-08 18:46:01 -0500

    logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,10 +55,18 @@ exclude_patterns = []
 #
 html_theme = "sphinx_rtd_theme"
 
+
+html_logo = "_static/logo.png"
+
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+html_theme_options = {
+    'logo_only': True
+}
 
 # -- Preventing member skips -------------------------------------------------
 
