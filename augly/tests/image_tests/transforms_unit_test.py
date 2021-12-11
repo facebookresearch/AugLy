50121e54b15d1ee1c1b433b3fde056d54d0d3172
Tanuj Dhiman <56601466+tanujdhiman@users.noreply.github.com>
2021-12-11 13:18:34 +0530

    Update transforms_unit_test.py
    Added the function here too

--- augly/tests/image_tests/transforms_unit_test.py
+++ augly/tests/image_tests/transforms_unit_test.py
@@ -201,6 +201,9 @@ class TransformsImageUnitTest(BaseImageUnitTest):
 
     def test_ShufflePixels(self):
         self.evaluate_class(imaugs.ShufflePixels(factor=0.5), fname="shuffle_pixels")
+    
+    def test_Skew(self):
+        self.evaluate_class(imaugs.Skew(skew_factor=0.5, axis=0), fname="skew")
 
     def test_VFlip(self):
         self.evaluate_class(imaugs.VFlip(), fname="vflip")
