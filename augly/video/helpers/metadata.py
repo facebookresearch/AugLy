cb4cbc1b2ee868c534fc4005fba8c56076b25751
Zoe Papakipos <zoep@fb.com>
2021-12-15 01:41:02 -0800

    Update copyright headers to Meta
    Summary:
    New recommended headers found here: https://www.internalfb.com/intern/wiki/Open_Source/Licenses/Platform_License/
    
    ```
    grep -rl "Copyright (c) Facebook Platforms, Inc. and its affiliates." | xargs sed -i 's/Copyright (c) Meta Platforms, Inc. and affiliates./Copyright (c) Meta Platforms, Inc. and affiliates.\n# All rights reserved.\n# \n# This source code is licensed under the license found in the\n# LICENSE file in the root directory of this source tree./g'
    ```
    <Deleted comments from header in LICENSE file>
    ```
    arc lint
    ```
    
    this_is_facebook_meta
    
    Differential Revision: D33123171
    
    fbshipit-source-id: 5db7603b48b78882af6c50ac8afdf1b36ec6e9ef

--- augly/video/helpers/metadata.py
+++ augly/video/helpers/metadata.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-# Copyright (c) Facebook, Inc. and its affiliates.
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
 
 from copy import deepcopy
 from typing import Any, Dict, List, Optional, Tuple
