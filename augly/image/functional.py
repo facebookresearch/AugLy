3f942f75d2690c8878dce556c83f764d30bcc6d6
Zoe Papakipos <zpapakipos@users.noreply.github.com>
2021-12-17 11:20:56 +0100

    Update augly/image/functional.py

--- augly/image/functional.py
+++ augly/image/functional.py
@@ -2428,7 +2428,7 @@ def skew(
         **func_kwargs,
     )
 
-    return imutils.ret_and_save_image(aug_image, output_path, src_mode)  # noqa
+    return imutils.ret_and_save_image(aug_image, output_path, src_mode)  # pyre-ignore
 
 
 def vflip(
