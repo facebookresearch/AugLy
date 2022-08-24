eba8d7ecc46d739d88d076f093b14c9477293d53
Gheorghe Postelnicu <gpost@fb.com>
2022-08-24 07:42:32 -0700

    Added optional src_id to segment.
    Differential Revision: https://www.internalfb.com/diff/D38073056?entry_point=27
    
    fbshipit-source-id: fd831070c1602a84965cda7fafec5d6096f36eaa

--- augly/utils/classes.py
+++ augly/utils/classes.py
@@ -5,9 +5,13 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import NamedTuple
+from typing import NamedTuple, Optional
 
 
 class Segment(NamedTuple):
     start: float
     end: float
+    src_id: Optional[str] = None
+
+    def delta(self, start_delta: float, end_delta: float):
+        return Segment(self.start + start_delta, self.end + end_delta, self.src_id)
