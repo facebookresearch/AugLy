e20980e60773805f182ef00fafc697369588f8e6
generatedunixname89002005307016 <generatedunixname89002005307016@meta.com>
2024-01-26 18:54:23 -0800

    suppress errors in `aml/augly/text`
    Differential Revision: D53150504
    
    fbshipit-source-id: f8af21313ffbb9d6cefab363c1cb60f67f910413

--- augly/text/augmenters/fun_fonts.py
+++ augly/text/augmenters/fun_fonts.py
@@ -100,7 +100,6 @@ class FunFontsAugmenter(Augmenter):
 
     def substitute(self, data: str) -> str:
         tokens = tokenize(data)
-        # pyre-fixme[20]: Argument `k` expected.
         font = random.sample(self.fonts, 1)[0]
         results = []
 
@@ -120,7 +119,6 @@ class FunFontsAugmenter(Augmenter):
                     continue
 
                 if self.vary_fonts:
-                    # pyre-fixme[20]: Argument `k` expected.
                     font = random.sample(self.fonts, 1)[0]
 
                 results.append(self.apply_font(token, font, Method.WORD))
@@ -136,7 +134,6 @@ class FunFontsAugmenter(Augmenter):
 
                 for char in chars:
                     if self.vary_fonts:
-                        # pyre-fixme[20]: Argument `k` expected.
                         font = random.sample(self.fonts, 1)[0]
                     if char_idx not in aug_char_idxes:
                         result += char
