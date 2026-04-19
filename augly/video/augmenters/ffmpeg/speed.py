e07517c9147aac95bf183f98b0d44f606627d81f
generatedunixname1228101065403401 <noreply+1228101065403401@fb.com>
2026-04-19 03:14:35 -0700

    Fix PYRE_STRICT issue in fbcode/aml/augly/video/augmenters/ffmpeg/speed.py
    Differential Revision: D101525543
    
    fbshipit-source-id: 57fc2bb3c1f23fc2157f21e16eb330a6d763de19

--- augly/video/augmenters/ffmpeg/speed.py
+++ augly/video/augmenters/ffmpeg/speed.py
@@ -5,6 +5,7 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
+# pyre-strict
 
 from augly.video.augmenters.ffmpeg.base_augmenter import BaseVidgearFFMPEGAugmenter
 
